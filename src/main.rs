c4f93ba5b8ef3893a5f4ed4d7cbb4afec7b6c3af
Andreas Rehn <rehn.andreas86@gmail.com>
2022-07-30 08:18:21 +0200

    add health endpoint
    provide health endpoint for monitoring

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#[macro_use] extern crate rocket;
+
+#[rocket::get("/health")]
+pub fn health() {}
+
+#[launch]
+fn rocket() -> _ {
+    let figment = rocket::Config::figment()
+        .merge(("port", 8080));
+
+    rocket::custom(figment)
+        .mount("/", routes![health])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rocket::http::Status;
+
+    #[test]
+    fn test_health() {
+        use rocket::local::blocking::Client;
+        let client = Client::tracked(rocket()).unwrap();
+        let resp = client.get("/health").dispatch();
+        assert_eq!(resp.status(), Status::Ok);
+    }
+}
